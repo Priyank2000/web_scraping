a9a55e2fe1c088b2ba7044bd37c6d3866ecd244a
Priyank2000 <55912392+Priyank2000@users.noreply.github.com>
2023-02-22 19:04:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,12 +14,22 @@ job = []
 address = []
 phone_no = []
 email_id = []
-
+school = []
+address = []
+state = []
+zip = []
 
 for website in webpages:
     result = requests.get(website)
     soup = BeautifulSoup(result.text, 'lxml')
 
+    school_name = soup.title.string.split("|")[-1].strip()
+    full_address = [a.text.strip() for a in soup.select("p[class='address']")]
+    full_address_new = full_address[-1].split("\n")
+    add = full_address_new[0].strip()
+    state_name = full_address_new[1].split(",")[0].strip()
+    zip_Code = full_address_new[1].split(",")[-1].strip()
+    
     for i in soup.select('.title'):
         name = i.get_text()
         a = name.split(',')
@@ -34,11 +44,6 @@ for website in webpages:
         c = job_title.replace('\n','')
         job.append(c) 
 
-    # for k in soup.select('.field-content span'):
-    #     loc = k.get_text()
-    #     b = loc.replace('\n',' ')
-    #     address.append(b)
-
     for l in soup.select('.field .phone a'):
         phone = l.get_text()
         phone_no.append(phone)
@@ -47,7 +52,13 @@ for website in webpages:
         email = m.get_text()
         email_id.append(email)
 
+for s in range(len(job)):
+    school.append(school_name)
+    address.append(add)
+    state.append(state_name)
+    zip.append(zip_Code)
 
-dic = {'First Name': first_name, 'Last Name': last_name, 'Job Title': job, 'Phone No': phone_no, 'Email Id': email_id}
+dic = {'First Name': first_name, 'Last Name': last_name, 'Job Title': job, 'Phone No': phone_no, 'Email Id': email_id, 'School Name': school, 'Address': address, 'State': state, 'zip': zip}
 df = pd.DataFrame(dic)
 df.to_csv('output.csv', index = False)
+
